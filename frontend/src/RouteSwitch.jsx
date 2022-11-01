4ffa5b8859b3ecc2e77d7c06e88ba877acfe31d8
Brilynd Madeya <bMadeya@flexicon.com>
2022-11-01 14:12:44 -0400

    Started Leaderboard Table css

--- frontend/src/RouteSwitch.jsx
+++ frontend/src/RouteSwitch.jsx
@@ -3,6 +3,7 @@ import App from "./App";
 // import Profile from "./Profile";
 import Gamemode from "./Gamemode";
 import Freeplay from "./Freeplay";
+import LeaderboardScreen from "./LeaderboardScreen";
 const RouteSwitch = () => {
   return (
     <BrowserRouter>
@@ -10,6 +11,7 @@ const RouteSwitch = () => {
         <Route path="/" element={<App />} />
         <Route path="/Gamemode/:id" element={<Gamemode/>}/>
         <Route path="/Freeplay/:id" element={<Freeplay/>}/>
+        <Route path = "/Leaderboard" element={<LeaderboardScreen/>}/>
       </Routes>
     </BrowserRouter>
   );
