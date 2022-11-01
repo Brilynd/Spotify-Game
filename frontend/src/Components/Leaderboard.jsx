58b784f0f12446241c6309c16a8f8922a9a7ee9d
Brilynd Madeya <bMadeya@flexicon.com>
2022-11-01 15:29:37 -0400

    Updated Scoreboard WIP

--- frontend/src/Components/Leaderboard.jsx
+++ frontend/src/Components/Leaderboard.jsx
@@ -1,34 +1,41 @@
 // {Headers,Data}
+import React from "react"
 import "./Leaderboard.css"
 const Leaderboard = () =>{
     const Headers = ['Index','Date','Artist','Username','Score']
-    const Data = [{Username:"Brilynd",Artist:"JuiceWrld",Date:"11/1/2022",Score:20},{Username:"Brilynd",Artist:"JuiceWrld",Date:"11/1/2022",Score:15}]
+    const Data = [{Username:"Brilynd Madeya",Artist:"JuiceWrld",Date:"11/1/2022",Score:20},{Username:"Brilynd Madeya",Artist:"JuiceWrld",Date:"11/1/2022",Score:15}]
 return(
-    <div>
-        <table id="Table-container">
-        <div id="Table-title">
-            <h1>Leaderboard</h1>
-        </div>
-            <tr>
-                {Headers.map(item=>{
-                    return(
-                    <th id="Table-header">{item}</th>
-                    )
-                })}
-                </tr>
-                {Data.map((item,index)=>{
-                    return(
-                    <tr id="Table-item">
-                    <td>{index}</td>
-                    <td>{item.Date}</td>
-                    <td>{item.Artist}</td>
-                    <td>{item.Username}</td>
-                    <td>{item.Score}</td>
-                    </tr>
-                    )
-                })}
-        </table>
-    </div>
+    <table class="content-table">
+    <thead>
+      <tr>
+        <th>Rank</th>
+        <th>Name</th>
+        <th>Points</th>
+        <th>Team</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>Domenic</td>
+        <td>88,110</td>
+        <td>dcode</td>
+      </tr>
+      <tr class="active-row">
+        <td>2</td>
+        <td>Sally</td>
+        <td>72,400</td>
+        <td>Students</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>Nick</td>
+        <td>52,300</td>
+        <td>dcode</td>
+      </tr>
+    </tbody>
+  </table>
+  
 )
 }
 export default Leaderboard
