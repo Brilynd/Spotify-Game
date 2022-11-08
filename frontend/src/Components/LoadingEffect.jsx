9043398827bcd9cdf04473358eb8efc2e52313dd
Nate Kaval <natekav26@gmail.com>
2022-11-07 22:10:08 -0500

    Loading screen components added and implemented

---
+++ frontend/src/Components/LoadingEffect.jsx
@@ -0,0 +1,18 @@
+import "./LoadingEffect.css"
+const LoadingEffect = () =>{
+    return(
+    <div class="center">
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+      </div>
+    )
+}
+export default LoadingEffect
